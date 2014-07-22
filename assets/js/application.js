3631dca9c7782e895e9ca79719de376558172af4
Kasey <yesakmac@gmail.com>
2014-07-22 07:55:51 -0500

    added site
    Added site, still needs cleanup.

---
+++ assets/js/application.js
@@ -0,0 +1,48 @@
+﻿function check_css() {
+   var m = document.getElementById('nerdmode').checked
+   alert(m)
+   if (m == true) {
+       changeCSS('assets/css/bootstrap.css', 0);
+   }
+   else {
+       changeCSS('assets/css/orig/bootstrap.css', 0);
+   };
+};
+
+function changeCSS(cssFile, cssLinkIndex) {
+
+    var oldlink = document.getElementsByTagName("link").item(cssLinkIndex);
+
+    var newlink = document.createElement("link");
+    newlink.setAttribute("rel", "stylesheet");
+    newlink.setAttribute("type", "text/css");
+    newlink.setAttribute("href", cssFile);
+
+    document.getElementsByTagName("head").item(0).replaceChild(newlink, oldlink);
+}
+
+
+function check_cookie() {
+    var x;
+    x = getCookie("style");
+    alert(x);
+
+};
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1);
+        if (c.indexOf(name) != -1) return c.substring(name.length, c.length);
+    }
+    return "";
+}
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires=" + d.toGMTString();
+    document.cookie = cname + "=" + cvalue + "; " + expires;
+}
